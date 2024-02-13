f8999b150da55d48366fa8dbc8687f39a1dc53ea
JositoMontilva <jmontilva@correo.unimet.edu.ve>
2024-02-13 11:32:28 -0400

    primer commit

---
+++ Proyecto-Hormiga_jose/src/main/java/proyecto/hormiga_jose_montilva/ProyectoHormiga_jose.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proyecto.hormiga_jose_montilva;
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
+/**
+ *
+ * @author Jose Gabriel
+ */
+public class ProyectoHormiga_jose {
+    public static void main(String[] args) {
+        interfaz c = new interfaz();
+        c.setVisible(true);
+        
+    }
+
+    
+}
+
